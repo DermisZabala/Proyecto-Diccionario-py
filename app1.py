5f17d57be20070491dc336a77b70a442d79be8b5
DermisZabala <dermiszabala@gmail.com>
2024-11-11 00:14:10 -0400

    AGREGANDO NUEVAS FUNCIONALIDADES, ENVIAR DEFINICION A WHATSAPP, ENUMERANDO LAS DEFINICIONES, SALIENDO DEL PROGRAMA Y LIMPIANDO LA CONSOLA

--- app1.py
+++ app1.py
@@ -1,6 +1,7 @@
 import json
 from difflib import SequenceMatcher, get_close_matches
-
+import os
+import pywhatkit
 dato = json.load(open("diccionario.json"))
 
 
@@ -20,16 +21,35 @@ def significado(p):
     else:
         return "La palabra no existe. Porfavor compruebalo dos veces"
 
-palabra = input("Ingrese una palabra: ").lower()
+mensajeWhatsapp=[]
 
-#print(SequenceMatcher(None, "hola", "hola").ratio())
 
-#print(get_close_matches("rainn", ["help", "pyramid", "rain"]))
+palabra = input("Escriba HAMEL para limpiar la pantalla y salir\nIngrese una palabra: ").lower()
 
-salida = significado(palabra)
+while True:
+    if palabra.lower() == "hamel":
+        os.system("cls")
+        break
+    else:
+        salida = significado(palabra)
 
-if type(salida)==list:
-    for i in salida:
-        print(i)
-else:
-    print(salida)
+    if type(salida)==list:
+        contador = 1
+        for i in salida:
+            mensajeWhatsapp.append("{}. {}".format(contador, i))
+            contador+=1
+        
+        mensaje = "\n".join(mensajeWhatsapp)
+        
+        try:            
+            print("Mensaje que se enviara\n" + mensaje)
+            # enviando el mensaje inmediatamente
+            pywhatkit.sendwhatmsg_instantly("+18297940243", mensaje, wait_time=4)  # espera 4 segundos para abrir el whatsapp
+            print("Mensaje Enviado") 
+        except Exception as e:
+            print(f"Ocurrió un error: {e}")
+
+    else: 
+        print(salida)
+
+    palabra = input("\nEscriba HAMEL para limpiar la pantalla y salir\nIngrese una palabra: ").lower()
